d0137f14f257609ee608efb0f429422bdd416160
P3IBOL <pablofol2003@gmail.com>
2025-01-29 21:14:01 +0100

    Minor changes in fully circular traduction

--- PWA-Signos/src/components/UploadImage.jsx
+++ PWA-Signos/src/components/UploadImage.jsx
@@ -167,12 +167,12 @@ const distanceTo = (x1, y1, x2, y2) => { // Distance between two points
     return Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2))
 };
 
-const findMyArro = (x, y, rot1, rot2, graphemes) => {
+const findMyArro = (x, y, rotations, graphemes) => {
     let minDist = 1
     let rotNearestArro = undefined
   graphemes.forEach((grapheme) => {
     if (grapheme["tags"]["CLASS"] === "ARRO" &&
-       (grapheme["tags"]["ROT"] === rot1 || grapheme["tags"]["ROT"] === rot2)) {
+       rotations.includes(grapheme["tags"]["ROT"])) {
         
         let dist = distanceTo(x, y, grapheme['box'][0], grapheme['box'][1])
         if (dist < minDist){
@@ -193,55 +193,63 @@ const arcToSignotation = (arc, graphemes) => {
   if (shape[1] === 'f') { // El movimiento es un circulo completo
     switch (rot) {
         case "N":
-            arroRot = findMyArro(arc['box'][0], arc['box'][1], 'W', 'E', graphemes)
+            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['SE', 'E', 'W', 'SW'], graphemes)
             if (shape[0] === 's')
                 return arroRot === 'W' ? '(B,X)' : '(B,Y)'
             
             return arroRot === 'W' ? '(L,X)' : '(L,Y)'
         case "NE":
-            arroRot = findMyArro(arc['box'][0], arc['box'][1], 'NW', 'SE', graphemes)
+            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['W', 'NW', 'S', 'SE'], graphemes)
             if (shape[0] === 's')
                 return arroRot === 'NW' ? '(B,X)' : '(B,Y)'
             
             return arroRot === 'NW' ? '(L,X)' : '(L,Y)'
         case "E":
-            arroRot = findMyArro(arc['box'][0], arc['box'][1], 'N', 'S', graphemes)
+            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['N', 'NW', 'S', 'SW'], graphemes)
             if (shape[0] === 's')
                 return arroRot === 'N' ? '(X,F)' : '(X,B)'
             
             return arroRot === 'N' ? '(X,H)' : '(X,L)'
         case "SE":
-            arroRot = findMyArro(arc['box'][0], arc['box'][1], 'NE', 'SW', graphemes)
+            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['N', 'NE', 'SW', 'W'], graphemes)
             if (shape[0] === 's')
                 return arroRot === 'NE' ? '(F,Y)' : '(F,Y)'
             
             return arroRot === 'NE' ? '(H,Y)' : '(H,X)'
         case "NW": 
-            arroRot = findMyArro(arc['box'][0], arc['box'][1], 'NE', 'SW', graphemes)
+            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['NE', 'SW'], graphemes)
             if (shape[0] === 's')
                 return arroRot === 'W' ? '(B,X)' : '(B,Y)'
             
             return arroRot === 'W' ? '(L,X)' : '(L,Y)'
         case "W":
-            arroRot = findMyArro(arc['box'][0], arc['box'][1], 'N', 'S', graphemes)
+            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['N', 'S'], graphemes)
             if (shape[0] === 's')
                 return arroRot === 'W' ? '(Y,B)' : '(Y,F)'
             
             return arroRot === 'W' ? '(Y,L)' : '(Y,H)'
         case "SW":
-            arroRot = findMyArro(arc['box'][0], arc['box'][1], 'NW', 'SE', graphemes)
+            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['NW', 'SE'], graphemes)
             if (shape[0] === 's')
                 return arroRot === 'W' ? '(F,Y)' : '(F,X)'
             
             return arroRot === 'W' ? '(H,Y)' : '(H,X)'
         case "S":
-            arroRot = findMyArro(arc['box'][0], arc['box'][1], 'W', 'E', graphemes)
+            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['W', 'E'], graphemes)
             if (shape[0] === 's')
                 return arroRot === 'W' ? '(F,Y)' : '(F,X)'
             
             return arroRot === 'W' ? '(H,Y)' : '(H,X)'
+        default:
+          break
       }
+
+
+
+
   }
+
+
 };
 
 const responseToSignotation = (response) => {
