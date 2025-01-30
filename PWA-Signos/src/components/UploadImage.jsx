61304e153b9a70fe53498f617c91416ea716960b
Pablo <pablofol2003@gmail.com>
2025-01-30 08:34:02 +0100

    Fully circular traduction done

--- PWA-Signos/src/components/UploadImage.jsx
+++ PWA-Signos/src/components/UploadImage.jsx
@@ -195,58 +195,54 @@ const arcToSignotation = (arc, graphemes) => {
         case "N":
             arroRot = findMyArro(arc['box'][0], arc['box'][1], ['SE', 'E', 'W', 'SW'], graphemes)
             if (shape[0] === 's')
-                return arroRot === 'W' ? '(B,X)' : '(B,Y)'
+                return (arroRot === 'W' || arroRot === 'SW') ? '(B,X)' : '(B,Y)'
             
-            return arroRot === 'W' ? '(L,X)' : '(L,Y)'
+            return (arroRot === 'W' || arroRot === 'SW') ? '(L,X)' : '(L,Y)'
         case "NE":
             arroRot = findMyArro(arc['box'][0], arc['box'][1], ['W', 'NW', 'S', 'SE'], graphemes)
             if (shape[0] === 's')
-                return arroRot === 'NW' ? '(B,X)' : '(B,Y)'
+                return (arroRot === 'NW' || arroRot === 'W') ? '(B,X)' : '(B,Y)'
             
-            return arroRot === 'NW' ? '(L,X)' : '(L,Y)'
+            return (arroRot === 'NW' || arroRot === 'W') ? '(L,X)' : '(L,Y)'
         case "E":
             arroRot = findMyArro(arc['box'][0], arc['box'][1], ['N', 'NW', 'S', 'SW'], graphemes)
             if (shape[0] === 's')
-                return arroRot === 'N' ? '(X,F)' : '(X,B)'
+                return (arroRot === 'N' || arroRot === 'NW') ? '(X,F)' : '(X,B)'
             
-            return arroRot === 'N' ? '(X,H)' : '(X,L)'
+            return (arroRot === 'N' || arroRot === 'NW') ? '(X,H)' : '(X,L)'
         case "SE":
             arroRot = findMyArro(arc['box'][0], arc['box'][1], ['N', 'NE', 'SW', 'W'], graphemes)
             if (shape[0] === 's')
-                return arroRot === 'NE' ? '(F,Y)' : '(F,Y)'
+                return (arroRot === 'NE' || arroRot === 'N') ? '(F,Y)' : '(F,Y)'
             
-            return arroRot === 'NE' ? '(H,Y)' : '(H,X)'
+            return (arroRot === 'NE' || arroRot === 'N')? '(H,Y)' : '(H,X)'
         case "NW": 
-            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['NE', 'SW'], graphemes)
+            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['E', 'NE', 'S', 'SW'], graphemes)
             if (shape[0] === 's')
-                return arroRot === 'W' ? '(B,X)' : '(B,Y)'
+                return (arroRot === 'S' || arroRot === 'SW') ? '(B,X)' : '(B,Y)'
             
-            return arroRot === 'W' ? '(L,X)' : '(L,Y)'
+            return (arroRot === 'S' || arroRot === 'SW') ? '(L,X)' : '(L,Y)'
         case "W":
-            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['N', 'S'], graphemes)
+            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['N', 'NE', 'S', 'SE'], graphemes)
             if (shape[0] === 's')
-                return arroRot === 'W' ? '(Y,B)' : '(Y,F)'
+                return (arroRot === 'S' || arroRot === 'SE') ? '(Y,B)' : '(Y,F)'
             
-            return arroRot === 'W' ? '(Y,L)' : '(Y,H)'
+            return (arroRot === 'S' || arroRot === 'SE') ? '(Y,L)' : '(Y,H)'
         case "SW":
-            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['NW', 'SE'], graphemes)
+            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['N', 'NW', 'E','SE'], graphemes)
             if (shape[0] === 's')
-                return arroRot === 'W' ? '(F,Y)' : '(F,X)'
+                return (arroRot === 'E' || arroRot === 'SE') ? '(F,Y)' : '(F,X)'
             
-            return arroRot === 'W' ? '(H,Y)' : '(H,X)'
+            return (arroRot === 'E' || arroRot === 'SE') ? '(H,Y)' : '(H,X)'
         case "S":
-            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['W', 'E'], graphemes)
+            arroRot = findMyArro(arc['box'][0], arc['box'][1], ['W', 'NW', 'E', 'NE'], graphemes)
             if (shape[0] === 's')
-                return arroRot === 'W' ? '(F,Y)' : '(F,X)'
+                return (arroRot === 'E' || arroRot === 'NE') ? '(F,Y)' : '(F,X)'
             
-            return arroRot === 'W' ? '(H,Y)' : '(H,X)'
+            return (arroRot === 'E' || arroRot === 'NE') ? '(H,Y)' : '(H,X)'
         default:
           break
       }
-
-
-
-
   }
 
 
