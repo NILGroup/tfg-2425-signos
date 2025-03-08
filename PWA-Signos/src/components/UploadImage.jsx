c749bec8d0b2bb87bacde702620505520b2e3f12
Sandra <sandracg1234@gmail.com>
2025-03-08 21:18:52 +0100

    styling videos Co-authored-by: P3IBOL711 <P3IBOL711@users.noreply.github.com>

--- PWA-Signos/src/components/UploadImage.jsx
+++ PWA-Signos/src/components/UploadImage.jsx
@@ -13,12 +13,14 @@ const UploadImage = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
   const [text, setText] = useState(null);
   const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImageName, setSelectedImageName] = useState(null);
   const input = useRef(null);
 
   const handleFileSelect = (event) => {
     const file = event.target.files[0];
     setSelectedFile(file);
     setSelectedImage(URL.createObjectURL(file));
+    setSelectedImageName(file.name);
   };
 
   const handleFileUpload = () => {
@@ -64,47 +66,53 @@ const UploadImage = () => {
 
     <div className="buttons">
 
-      {text && <div clasName="flex flex-col"> 
-              <h1 className="signotacion text-[#4682A9] font-bold text-3xl mb-4"> SIGNOTACIÓN </h1> 
+      { text && <div className="flex flex-col gap-4 mt-10"> 
+              <h1 className="signotacion text-[#4682A9] font-bold text-3xl"> SIGNOTACIÓN </h1> 
               <h1 className="signotacion text-[#4682A9] font-bold text-2xl"> {text}</h1> 
-              </div>}
+              </div> }
+    
+      <div className="flex flex-row gap-30 mt-20">
+        <div className="flex flex-col gap-4">
+          {selectedImage && <div className="flex flex-col gap-1"> 
+                            <img className="border-4 rounded-xl border-[#4682A9] border-solid" src={selectedImage} alt="Signoescritura"/> 
+                            <p className="text-[#4682A9] font-bold text-lg"> {selectedImageName} </p> 
+                            </div>}
 
-      {selectedImage && <img src={selectedImage} alt="Signoescritura"/>}
+          <div className="flex justify-center gap-10">
+            <input
+              className="hidden"
+              type="file"
+              accept="image/*"
+              max={1}
+              ref={input}
+              onChange={handleFileSelect}
+            />
 
-      <div className="flex gap-10">
-        <input
-          className="hidden"
-          type="file"
-          accept="image/*"
-          max={1}
-          ref={input}
-          onChange={handleFileSelect}
-        />
+            <button onClick={() => input.current.click()} className="group border-[#4682A9] border-6 hover:bg-[#4682A9] rounded-full w-20 h-20">
+              <img src={uploadIcon} alt="Upload Icon" className="scale-150 p-2 pt-2.25 group-hover:brightness-0 group-hover:invert"/>
+            </button>
 
-        <button onClick={() => input.current.click()} className="group border-[#4682A9] border-6 hover:bg-[#4682A9] rounded-full w-20 h-20">
-          <img src={uploadIcon} alt="Upload Icon" className="scale-150 p-2 pt-2.25 group-hover:brightness-0 group-hover:invert"/>
-        </button>
-
-        <button onClick={handleFileUpload} className="group border-[#4682A9] border-6 hover:bg-[#4682A9] rounded-full w-20 h-20">
-          <img src={checkIcon} alt="Send image" className="scale-180 p-2.75 pt-3.25 group-hover:brightness-0 group-hover:invert"/>
-        </button>
-      
-      </div>
-
-      <div className="videos">
-        
-        {videos !== null &&
-          videos.map((_, index) => {
-            return (
-              <Video
-                key={index}
-                index={index}
-                info={videos[index]}
-                updateSelected={setSelectedVideo}
-                selectedVideo={selectedVideo}
-              ></Video>
-            );
-          })}
+            <button onClick={handleFileUpload} className="group border-[#4682A9] border-6 hover:bg-[#4682A9] rounded-full w-20 h-20">
+              <img src={checkIcon} alt="Send image" className="scale-180 p-2.75 pt-3.25 group-hover:brightness-0 group-hover:invert"/>
+            </button>
+          
+          </div>
+        </div>
+        {videos && (
+          <div className="videos border-4 rounded-xl border-[#4682A9] border-solid w-250 h-127">
+            {videos.map((_, index) => {
+              return ( 
+                <Video 
+                  key={index}
+                  index={index}
+                  info={videos[index]}
+                  updateSelected={setSelectedVideo}
+                  selectedVideo={selectedVideo}
+                />
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
--- PWA-Signos/src/components/Video.jsx
+++ PWA-Signos/src/components/Video.jsx
@@ -22,12 +22,14 @@ const Video = ({index, info, updateSelected, selectedVideo}) => {
     }
 
     return (
-        <div onClick={handleClick} className={className}>
-            <video className="video" width="160" height="120">
+        <div onClick={handleClick} className={`${className} flex flex-row`}>
+            <video className="video w-50 h-30">
                 <source src={info['video']} type="video/mp4"></source>
             </video>
-            <h1>{info['gloss']}</h1>
-            <h2>{info['notation']}</h2>
+            <div className="flex flex-col gap-1 justify-center">
+                <h1 className="text-[#4682A9] font-bold text-lg">{info['gloss']}</h1>
+                <p className="text-[#4682A9] font-bold text-base">{info['notation']}</p>
+            </div>
         </div>
     );
 }
