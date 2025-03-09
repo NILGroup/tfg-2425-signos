4cc66c479baf1625b45d47049f968ae6b33574c1
Sandra <sandracg1234@gmail.com>
2025-03-09 22:08:56 +0100

    loading done

--- PWA-Signos/src/components/UploadImage.jsx
+++ PWA-Signos/src/components/UploadImage.jsx
@@ -11,20 +11,57 @@ const UploadImage = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [videos, setVideos] = useState(null);
   const [selectedVideo, setSelectedVideo] = useState(null);
-  const [text, setText] = useState(null);
+  const [signotationText, setSignotationText] = useState(null);
   const [selectedImage, setSelectedImage] = useState(null);
   const [selectedImageName, setSelectedImageName] = useState(null);
 
   const [loading, setLoading] = useState(null);
-
 
   const input = useRef(null);
 
   const handleFileSelect = (event) => {
     const file = event.target.files[0];
     setSelectedFile(file);
+    setSignotationText(null);
+    setVideos(null);
     setSelectedImage(URL.createObjectURL(file));
     setSelectedImageName(file.name);
+  };
+
+  const uploadImage = async (image) => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        VISSE_BACKEND_URL /*"http://localhost:3999/recognize/raw"*/,
+        {
+          method: "POST",
+          body: image,
+        }
+      );
+      const responseData = await response.json();
+      console.log(responseData);
+      const signotation = await responseToSignotation(responseData);
+      await setSignotationText(signotation);
+      const url = new URL(
+        SIGNARIO_URL +
+          new URLSearchParams({
+            s: signotation,
+            // l es opcional
+          })
+      );
+
+      const videosResponse = await fetch(url, {
+        method: "GET",
+      });
+      const videosData = await videosResponse.json();
+      console.log(videosData);
+      setVideos(videosData["signs"]);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
+    finally {
+      setLoading(false);
+    }
   };
 
   const handleFileUpload = () => {
@@ -32,50 +69,16 @@ const UploadImage = () => {
     const image = new FormData();
     image.append("image", selectedFile);
     // Send selected image to Visse
-    const uploadImage = async () => {
-      try {
-        const response = await fetch(
-          VISSE_BACKEND_URL /*"http://localhost:3999/recognize/raw"*/,
-          {
-            method: "POST",
-            body: image,
-          }
-        );
-        const responseData = await response.json();
-        console.log(responseData);
-        const signotation = await responseToSignotation(responseData);
-        await setText(signotation);
-        const url = new URL(
-          SIGNARIO_URL +
-            new URLSearchParams({
-              s: signotation,
-              // l es opcional
-            })
-        );
-
-        const videosResponse = await fetch(url, {
-          method: "GET",
-        });
-        const videosData = await videosResponse.json();
-        console.log(videosData);
-        await setVideos(videosData["signs"]);
-      } catch (error) {
-        console.error("Error uploading file:", error);
-      }
-    };
-
-    setLoading(true);
-    uploadImage();
-    setLoading(false);
+    uploadImage(image);
   };
 
   return (
 
     <div className="buttons">
 
-      { text && <div className="flex flex-col gap-4 mt-10"> 
+      { signotationText && !loading && <div className="flex flex-col gap-4 mt-10"> 
               <h1 className="signotacion text-[#4682A9] font-bold text-3xl"> SIGNOTACIÓN </h1> 
-              <h1 className="signotacion text-[#4682A9] font-bold text-2xl"> {text}</h1> 
+              <h1 className="signotacion text-[#4682A9] font-bold text-2xl"> {signotationText}</h1> 
               </div> }
     
       <div className="flex flex-row gap-30 mt-20">
@@ -94,19 +97,40 @@ const UploadImage = () => {
               ref={input}
               onChange={handleFileSelect}
             />
-
+            
+            {/*Botón de Elegir imagen*/}
             <button onClick={() => input.current.click()} className="group border-[#4682A9] border-6 hover:bg-[#4682A9] rounded-full w-20 h-20">
               <img src={uploadIcon} alt="Upload Icon" className="scale-150 p-2 pt-2.25 group-hover:brightness-0 group-hover:invert"/>
             </button>
-
+            
+            {/*Botón de Tick*/}
             <button onClick={handleFileUpload} className="group border-[#4682A9] border-6 hover:bg-[#4682A9] rounded-full w-20 h-20">
               <img src={checkIcon} alt="Send image" className="scale-180 p-2.75 pt-3.25 group-hover:brightness-0 group-hover:invert"/>
             </button>
           
           </div>
         </div>
-        {loading && <h1>Loading...</h1>}
-        {videos && (
+
+        {loading && (
+          <h1 className="flex text-3xl text-[#4682A9] font-bold p-40 gap-5">
+            <svg className="w-6 h-6 animate-spin" viewBox="0 0 50 50">
+              <circle
+                className="stroke-current"
+                cx="25"
+                cy="25"
+                r="20"
+                fill="none"
+                strokeWidth="5"
+                strokeLinecap="round"
+                strokeDasharray="80, 200"
+                strokeDashoffset="0"
+              ></circle>
+            </svg>
+            Cargando vídeos...
+          </h1>
+        )}
+
+        {videos && !loading && (
           <div className="border-4 border-[#4682A9] rounded-xl w-250 h-127 overflow-hidden">
             <div className="flex flex-col overflow-y-scroll h-full 
                     [&::-webkit-scrollbar]:w-1.5
@@ -126,8 +150,8 @@ const UploadImage = () => {
               ))}
             </div>
           </div>
-        
         )}
+
       </div>
     </div>
   );
