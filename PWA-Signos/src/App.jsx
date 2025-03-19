51898fee19be721e9b3a9710601b2b3516b3cd1c
sandra-conde-gonzalez <sandracg1234@gmail.com>
2025-03-19 11:41:03 +0100

    trying duracion example

--- PWA-Signos/src/App.jsx
+++ PWA-Signos/src/App.jsx
@@ -10,8 +10,8 @@ function App() {
         <h1 className="text-4xl text-center text-[#4682A9] font-bold"> Traduciendo la SignoEscritura </h1>
         <hr className="mx-4 mt-3 h-0.5 bg-[#91C8E4] rounded-full border-none"></hr>
       </div>
-      {/*<SwitchMode className="theme-switcher"/>*/}
-      <Examples />
+      <SwitchMode className="theme-switcher"/>
+      {/*<Examples />*/}
     </>
   )
 }
