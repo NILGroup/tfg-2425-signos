4fd7ca304c7e197647fb62f5035466a4379b465b
P3IBOL <pablofol2003@gmail.com>
2025-03-19 21:15:24 +0100

    Switch button fixed Co-authored-by: sandra-conde-gonzalez <sandra-conde-gonzalez@users.noreply.github.com>

--- PWA-Signos/src/components/SwitchMode.jsx
+++ PWA-Signos/src/components/SwitchMode.jsx
@@ -10,21 +10,20 @@ const SwitchMode = ({isChecked, changeMode}) => {
       <div >
         <div className="flex justify-center">
           <div className="border-4 rounded-xl border-[#4682A9] border-solid inline-flex items-center overflow-hidden">
-            <button className={`group border-[#4682A9] border-r-4 border-stroke px-4 py-3 text-base font-medium text-dark last-of-type:border-r-0 hover:cursor-pointer ${isChecked ? "bg-[#4682A9]" : ""}`}>
+            <button onClick={handleModeChange} className={`border-[#4682A9] border-stroke px-4 py-3 hover:cursor-pointer ${!isChecked ? "bg-[#4682A9]" : "border-r-4 "}`}>
             <svg width="36px"
            height="36px"
             viewBox="-0.5 -0.5 25 25"
              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              stroke="#4682A9"
-              className={`${isChecked ? "group-hover:brightness-0 group-hover:invert" : ""}`}>
+            xmlns="http://www.w3.org/2000/svg"
+              >
           <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
           <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
           <g id="SVGRepo_iconCarrier">
             <path d="M14.2639 15.9375L12.5958 14.2834C11.7909 13.4851 11.3884 13.086 10.9266 12.9401C10.5204 12.8118 10.0838 12.8165 9.68048 12.9536C9.22188 13.1095 8.82814 13.5172 8.04068 14.3326L4.04409 18.2801M14.2639 15.9375L14.6053 15.599C15.4112 14.7998 15.8141 14.4002 16.2765 14.2543C16.6831 14.126 17.12 14.1311 17.5236 14.2687C17.9824 14.4251 18.3761 14.8339 19.1634 15.6514L20 16.4934M14.2639 15.9375L18.275 19.9565M18.275 19.9565C17.9176 20 17.4543 20 16.8 20H7.2C6.07989 20 5.51984 20 5.09202 19.782C4.71569 19.5903 4.40973 19.2843 4.21799 18.908C4.12796 18.7313 4.07512 18.5321 4.04409 18.2801M18.275 19.9565C18.5293 19.9256 18.7301 19.8727 18.908 19.782C19.2843 19.5903 19.5903 19.2843 19.782 18.908C20 18.4802 20 17.9201 20 16.8V16.4934M4.04409 18.2801C4 17.9221 4 17.4575 4 16.8V7.2C4 6.0799 4 5.51984 4.21799 5.09202C4.40973 4.71569 4.71569 4.40973 5.09202 4.21799C5.51984 4 6.07989 4 7.2 4H16.8C17.9201 4 18.4802 4 18.908 4.21799C19.2843 4.40973 19.5903 4.71569 19.782 5.09202C20 5.51984 20 6.0799 20 7.2V16.4934M17 8.99989C17 10.1045 16.1046 10.9999 15 10.9999C13.8954 10.9999 13 10.1045 13 8.99989C13 7.89532 13.8954 6.99989 15 6.99989C16.1046 6.99989 17 7.89532 17 8.99989Z" 
                 fillRule="evenodd"
                 clipRule="evenodd"
-                stroke="#4682A9" 
+                stroke={`${!isChecked ? "#c5dbe6" : "#4682A9"}`} 
                 strokeWidth="1.5" 
                 strokeLinecap="round" 
                 strokeLinejoin="round">
@@ -32,7 +31,7 @@ const SwitchMode = ({isChecked, changeMode}) => {
               </g>
           </svg>
             </button>
-            <button className={`border-[#4682A9] border-r-4 border-stroke px-4 py-3 text-base font-medium text-dark last-of-type:border-r-0 hover:cursor-pointer ${!isChecked ? "bg-[#4682A9]" : ""}`}>
+            <button onClick={handleModeChange} className={`border-[#4682A9] border-stroke px-4 py-3 hover:cursor-pointer ${isChecked ? "bg-[#4682A9]" : "border-l-4"}`}>
             <svg
               width="36px"
               height="36px"
@@ -47,14 +46,14 @@ const SwitchMode = ({isChecked, changeMode}) => {
                   fillRule="evenodd"
                   clipRule="evenodd"
                   d="M17.265 4.16231L19.21 5.74531C19.3978 5.9283 19.5031 6.17982 19.5015 6.44201C19.5 6.70421 19.3919 6.9545 19.202 7.13531L17.724 8.93531L12.694 15.0723C12.6069 15.1749 12.4897 15.2473 12.359 15.2793L9.75102 15.8793C9.40496 15.8936 9.10654 15.6384 9.06702 15.2943L9.18902 12.7213C9.19806 12.5899 9.25006 12.4652 9.33702 12.3663L14.15 6.50131L15.845 4.43331C16.1743 3.98505 16.7938 3.86684 17.265 4.16231Z"
-                  stroke="#4682A9"
+                  stroke={`${isChecked ? "#c5dbe6" : "#4682A9"}`}
                   strokeWidth="1.5"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 ></path>
                 <path
                   d="M5.5 18.2413C5.08579 18.2413 4.75 18.5771 4.75 18.9913C4.75 19.4056 5.08579 19.7413 5.5 19.7413V18.2413ZM19.2 19.7413C19.6142 19.7413 19.95 19.4056 19.95 18.9913C19.95 18.5771 19.6142 18.2413 19.2 18.2413V19.7413ZM14.8455 6.22062C14.6904 5.83652 14.2534 5.65082 13.8693 5.80586C13.4852 5.9609 13.2995 6.39796 13.4545 6.78206L14.8455 6.22062ZM17.8893 9.66991C18.2933 9.57863 18.5468 9.17711 18.4556 8.77308C18.3643 8.36904 17.9628 8.1155 17.5587 8.20678L17.8893 9.66991ZM5.5 19.7413H19.2V18.2413H5.5V19.7413ZM13.4545 6.78206C13.6872 7.35843 14.165 8.18012 14.8765 8.8128C15.6011 9.45718 16.633 9.95371 17.8893 9.66991L17.5587 8.20678C16.916 8.35198 16.3609 8.12551 15.8733 7.69189C15.3725 7.24656 15.0128 6.63526 14.8455 6.22062L13.4545 6.78206Z"
-                  fill="#4682A9"
+                  fill={`${isChecked ? "#c5dbe6" : "#4682A9"}`}
                 ></path>
               </g>
             </svg>
