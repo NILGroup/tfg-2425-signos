9a8bcc2e20c8a7c6007efe70016838753c2aa1de
Pablo <pablofol2003@gmail.com>
2025-03-21 11:06:24 +0100

    Enhance stemToSignotation to handle arrows on both sides for improved signotatio translation

--- PWA-Signos/src/translator/stemTranslator.js
+++ PWA-Signos/src/translator/stemTranslator.js
@@ -14,14 +14,17 @@ const stemToSignotation = (stem, arros) => {
 
             if(arro1 === undefined && arro2 === undefined) // Shoulders, waists, forearms or complex movements are ignored
                 break;
-            // Caso de que haya flecha a ambos lados PENDIENTE
-            if(arro1 != undefined && arro2 === undefined){
+            if(arro1 != undefined && arro2 === undefined){ // Arrow north
                 dir = shape === 's' ? 'F' : 'H';
                 rep = arro1[1];
             }
-            else if(arro2 != undefined && arro1 === undefined){
+            else if(arro2 != undefined && arro1 === undefined){ // Arrow south
                 dir = shape === 's' ? 'B' : 'L';
                 rep = arro2[1];
+            }
+            else{ // Arrow on both sides
+                dir = shape === 's' ? 'F' : 'H';
+                rep = true;
             }
             break;
         case 'NE':
@@ -30,14 +33,17 @@ const stemToSignotation = (stem, arros) => {
 
             if(arro1 === undefined && arro2 === undefined) // Shoulders, waists, forearms or complex movements are ignored
                 break;
-            // Caso de que haya flecha a ambos lados PENDIENTE
-            if(arro1 != undefined && arro2 === undefined){
+            if(arro1 != undefined && arro2 === undefined){ // Arrow north east
                 dir = shape === 's' ? 'FY' : 'HY';
                 rep = arro1[1];
             }
-            else if(arro2 != undefined && arro1 === undefined){
+            else if(arro2 != undefined && arro1 === undefined){ // Arrow south west
                 dir = shape === 's' ? 'BX' : 'LX';
                 rep = arro2[1];
+            }
+            else{ // Arrow on both sides
+                dir = shape === 's' ? 'FY' : 'HY';
+                rep = true;
             }
             break;
         case 'E': // Horizontal
@@ -46,14 +52,17 @@ const stemToSignotation = (stem, arros) => {
 
             if(arro1 === undefined && arro2 === undefined) // Shoulders, waists, forearms or complex movements are ignored
                 break;
-            // Caso de que haya flecha a ambos lados PENDIENTE
-            if(arro1 != undefined && arro2 === undefined){
+            if(arro1 != undefined && arro2 === undefined){ // Arrow east
                 dir = 'Y';
                 rep = arro1[1];
             }
-            else if(arro2 != undefined && arro1 === undefined){
+            else if(arro2 != undefined && arro1 === undefined){ // Arrow west
                 dir = 'X';
                 rep = arro2[1];
+            }
+            else{ // Arrow on both sides
+                dir = 'Y';
+                rep = true;
             }
             break;
         case 'SE':
@@ -62,14 +71,17 @@ const stemToSignotation = (stem, arros) => {
 
             if(arro1 === undefined && arro2 === undefined) // Shoulders, waists, forearms or complex movements are ignored
                 break;
-            // Caso de que haya flecha a ambos lados PENDIENTE
-            if(arro1 != undefined && arro2 === undefined){
+            if(arro1 != undefined && arro2 === undefined){ // Arrow north west
                 dir = shape === 's' ? 'FX' : 'HX';
                 rep = arro1[1];
             }
-            else if(arro2 != undefined && arro1 === undefined){
+            else if(arro2 != undefined && arro1 === undefined){ // Arrow south east
                 dir = shape === 's' ? 'BY' : 'LY';
                 rep = arro2[1];
+            }
+            else{
+                dir = shape === 's' ? 'FX' : 'HX';
+                rep = true;
             }
             break;
         default:
